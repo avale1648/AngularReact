e5c3b2caca6fc1733acaa822ed6a85705cd49ca4
Андрей Миронов <avale1648@gmail.com>
2024-02-16 14:18:21 +0300

    Update Homework 2023_10_27_1

--- Homeworks/Homework_2023_10_27_1/src/app/app.module.ts
+++ Homeworks/Homework_2023_10_27_1/src/app/app.module.ts
@@ -1,19 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//App
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//Angular Material
-import { MaterialModule} from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatNativeDateModule} from '@angular/material/core';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatGridListModule,
+    MatBadgeModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
